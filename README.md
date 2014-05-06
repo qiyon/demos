f295c2924ef94830000998874e4548d6f24b8d44
kiwong <heqiyon@gmail.com>
2014-05-06 21:41:17 +0800

    readme file

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ sxadmin
 =======
 
 The admin module of sx.
+
+详细内容请查看ReadMe.php
--- ReadMe.php
+++ ReadMe.php
@@ -9,4 +9,5 @@
  * 这个文件只是一个项目说明文件，用php文件注释是为了防止Web访问。
  *
  * 部署前请在./protected/config/netConfig.php修改相应的全局变量设置
+ * 运行时错误的话则先在 ./protected/下创建一个 runtime/ 文件夹
  */ 
