c3aebbeff0e8b9935eb42820748c8320829606b5
qiyon <heqiyon@gmail.com>
2015-03-10 14:14:49 +0000

    simple

--- src/index.php
+++ src/index.php
@@ -1,1 +1,34 @@
 <?php
+class pjson
+{
+    private $legal;
+    private $outJsonStr;
+
+    public function __construct($inputStr)
+    {
+
+    }
+
+    public function isLegal()
+    {
+
+    }
+
+    public function outStr()
+    {
+
+    }
+
+    public static function strFromStdin()
+    {
+        $getStr = "";
+        while (!feof(STDIN)){
+            $getStr .= fgets(STDIN); 
+        }
+        return $getStr;
+    }
+}
+
+
+$getArr = json_decode(pjson::strFromStdin() , true);
+print_r($getArr);
