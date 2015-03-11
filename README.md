b5a57eeacb5e2c6861b5ee05fc59214a7fb58651
qiyon <heqiyon@gmail.com>
2015-03-11 14:59:41 +0000

    pjson

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # pjson
-php json tool.
-Like python's mjson.
+php json tool, like python's mjson.
+
+Use in linux:
+```
+cp index.php /usr/local/bin/pjson
+chmod +x /usr/local/bin/pjson
+echo '{"json":"example"}' | pjson
+```
--- src/index.php
+++ src/index.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 class pjson
 {
@@ -6,17 +7,82 @@ class pjson
 
     public function __construct($inputStr)
     {
-
+        $getArr = json_decode($inputStr , true);
+        if(empty($getArr) && $getArr !== array()){
+            $this->legal = false;
+        }else{
+            $this->legal = true;
+            $this->dumpJson($getArr);
+        }
     }
 
     public function isLegal()
     {
+        return $this->legal;
+    }
 
+    private function addStr($miniStr , $loop = 0)
+    {
+        while ($loop > 0 ) {
+            $this->outJsonStr .= '    ';
+            $loop--;
+    }
+        $this->outJsonStr .= $miniStr;
+    }
+
+    public function dumpJson($getArr , $loop = 0)
+    {
+        if (!is_array($getArr)){
+            //int str bool
+            $this->addStr( $this->getTypeStr($getArr));
+            return ;
+        }
+        $countIn = 0;
+        $countAll = count($getArr);
+        if ($getArr === array()){
+            //empty []
+            $this->addStr('[]');
+            return;
+        }
+        if (array_keys($getArr) === range(0, $countAll-1)){
+            //[]
+            $this->addStr("[\n");
+            foreach ($getArr as $value) {
+                $this->addStr('' , $loop+1);
+                $this->dumpJson($value , $loop+1);
+                if ( $countIn != $countAll-1 ) $this->addStr(',');
+                $this->addStr("\n");
+                $countIn++;
+            }
+            $this->addStr(']' , $loop);
+        }else{
+            //{}
+            $this->addStr("{\n");
+            foreach ($getArr as $key => $value) {
+                $this->addStr('"'.$key.'":' , $loop+1);
+                $this->dumpJson($value , $loop+1);
+                if ( $countIn != $countAll-1 ) $this->addStr(',');
+                $this->addStr("\n");
+                $countIn++;
+            }
+            $this->addStr('}' , $loop);
+        }
+    }
+
+    private function getTypeStr($var)
+    {
+        if (is_int($var)){
+            return $var;
+        }else if(is_bool($var)){
+            return ($var) ? 'true' : 'false';
+        }else{
+            return '"'.$var.'"';
+        }
     }
 
     public function outStr()
     {
-
+        return $this->outJsonStr . "\n";
     }
 
     public static function strFromStdin()
@@ -29,6 +95,11 @@ class pjson
     }
 }
 
-
-$getArr = json_decode(pjson::strFromStdin() , true);
-print_r($getArr);
+$stdinStr = pjson::strFromStdin();
+$pj = new pjson($stdinStr);
+if ($pj->isLegal()){
+    echo $pj->outStr();
+}else{
+    echo "Invalid json string:\n";
+    echo $stdinStr;
+}
