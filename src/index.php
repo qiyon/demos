132f9d27d6c10b6b60b4dae2b384d5f94d10f332
qiyon <heqiyon@gmail.com>
2015-03-14 04:00:02 +0000

    new phar version

--- src/index.php
+++ src/index.php
@@ -1,101 +1,11 @@
 #!/usr/bin/env php
 <?php
-class pjson
-{
-    private $legal;
-    private $outJsonStr;
+require( __DIR__.'/pjson.php');
 
-    public function __construct($inputStr)
-    {
-        $getArr = json_decode($inputStr , true);
-        if(empty($getArr) && $getArr !== array()){
-            $this->legal = false;
-        }else{
-            $this->legal = true;
-            $this->dumpJson($getArr);
-        }
-    }
-
-    public function isLegal()
-    {
-        return $this->legal;
-    }
-
-    private function addStr($miniStr , $loop = 0)
-    {
-        while ($loop > 0 ) {
-            $this->outJsonStr .= '    ';
-            $loop--;
-        }
-        $this->outJsonStr .= $miniStr;
-    }
-
-    public function dumpJson($getArr , $loop = 0)
-    {
-        if (!is_array($getArr)){
-            //int str bool
-            $this->addStr( $this->getTypeStr($getArr));
-            return ;
-        }
-        $countIn = 0;
-        $countAll = count($getArr);
-        if ($getArr === array()){
-            //empty []
-            $this->addStr('[]');
-            return;
-        }
-        if (array_keys($getArr) === range(0, $countAll-1)){
-            //[]
-            $this->addStr("[\n");
-            foreach ($getArr as $value) {
-                $this->addStr('' , $loop+1);
-                $this->dumpJson($value , $loop+1);
-                if ( $countIn != $countAll-1 ) $this->addStr(',');
-                $this->addStr("\n");
-                $countIn++;
-            }
-            $this->addStr(']' , $loop);
-        }else{
-            //{}
-            $this->addStr("{\n");
-            foreach ($getArr as $key => $value) {
-                $this->addStr('"'.$key.'":' , $loop+1);
-                $this->dumpJson($value , $loop+1);
-                if ( $countIn != $countAll-1 ) $this->addStr(',');
-                $this->addStr("\n");
-                $countIn++;
-            }
-            $this->addStr('}' , $loop);
-        }
-    }
-
-    private function getTypeStr($var)
-    {
-        if (is_int($var)){
-            return $var;
-        }else if(is_bool($var)){
-            return ($var) ? 'true' : 'false';
-        }else{
-            return '"'.$var.'"';
-        }
-    }
-
-    public function outStr()
-    {
-        return $this->outJsonStr . "\n";
-    }
-
-    public static function strFromStdin()
-    {
-        $getStr = "";
-        while (!feof(STDIN)){
-            $getStr .= fgets(STDIN); 
-        }
-        return $getStr;
-    }
+$stdinStr = "";
+while (!feof(STDIN)){
+    $stdinStr .= fgets(STDIN); 
 }
-
-$stdinStr = pjson::strFromStdin();
 $pj = new pjson($stdinStr);
 if ($pj->isLegal()){
     echo $pj->outStr();
---
+++ src/toPhar.php
@@ -0,0 +1,2 @@
+<?php
+//todo phar create
