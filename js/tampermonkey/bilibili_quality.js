64fd8a2db9f94647228cf1a56eab35332e414f12
qiyon <heqiyon@gmail.com>
2017-08-05 00:18:35 +0800

    step && max_time

--- js/tampermonkey/bilibili_quality.js
+++ js/tampermonkey/bilibili_quality.js
@@ -9,11 +9,15 @@
 // ==/UserScript==
 (function() {
     'use strict';
+    var step_ms = 1000;
+    var max_time = 20 * 1000;
+    var counter = 0;
     selectQuality();
     function selectQuality(){
+        counter += step_ms;
         var list = $('.bilibili-player-video-btn-quality').find('.bpui-selectmenu-list-row');
         if (list.length === 0) {
-            setTimeout(selectQuality, 1000);
+            if (counter <= max_time) setTimeout(selectQuality, step_ms);
         } else {
             list.get(list.length - 1).click();
         }
