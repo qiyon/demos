3b70d71bc50527151b2e46b652a25efb30fda565
qiyon <heqiyon@gmail.com>
2018-03-22 23:18:22 +0800

    use json, and some fix

--- golang/http_file_upload/README.md
+++ golang/http_file_upload/README.md
@@ -6,7 +6,7 @@
 touch file.txt
 go run main.go
 ## An other terminal
-curl -X POST -F "name=xxx" -F "file=@file.txt" 127.0.0.1:8080/upload
+curl -X POST -F "name=xxx" -F "file=@file.txt" 127.0.0.1:8080
 ```
 
 ## Timeout Test
@@ -21,10 +21,10 @@ mkfile 512m file.txt
 curl rate limit
 
 ```
-curl -X POST -F "name=xxx" -F "file=@file.txt" --limit-rate 50m 127.0.0.1:8080/upload
+curl -X POST -F "name=xxx" -F "file=@file.txt" --limit-rate 50m 127.0.0.1:8080
 ```
 
 ## conclusion
 
-Golang http, resolve the html multipart/form-data input and file info, after get all data from client
+Golang http, default (`PostFormValue()`) resolve the html multipart/form-data input and file info, after get all data from client
 
--- golang/http_file_upload/main.go
+++ golang/http_file_upload/main.go
@@ -1,46 +1,51 @@
 package main
 
-import "fmt"
-import "net/http"
-import "time"
-import "os"
-import "io"
+import (
+	"fmt"
+	"net/http"
+	"time"
+	"os"
+	"io"
+	"encoding/json"
+)
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(time.Now().Unix())
-	fmt.Println("method:", r.Method)
+	var info = make(map[string]interface{})
+	info["start_time"] = time.Now().Unix()
+	info["method"] = r.Method
 	if r.Method == "POST" {
-		name := r.PostFormValue("name")
-		fmt.Println("name: ", name)
-		fmt.Println(time.Now().Unix())
+		info["form.name"] = r.PostFormValue("name")
+		info["form.parsed_time"] = time.Now().Unix()
 		file, handler, err := r.FormFile("file")
 		if err != nil {
-			fmt.Println(err)
-			return
+			info["error"] = err.Error()
+		} else {
+			defer file.Close()
+			store, err := os.OpenFile("./upload_default/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+			if err != nil {
+				info["error"] = err.Error()
+			} else {
+				defer store.Close()
+				io.Copy(store, file)
+			}
 		}
-		defer file.Close()
-		store, err := os.OpenFile("./upload_default/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer store.Close()
-		io.Copy(store, file)
-		fmt.Println(time.Now().Unix())
-		fmt.Println("Copy Complete!")
 	}
-	fmt.Fprintf(w, "Receive %s", r.Method)
+	info["end_time"] = time.Now().Unix()
+	data, _ := json.Marshal(info);
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "%s", string(data))
 }
 
 func main() {
 	//http.HandleFunc("/upload", uploadHandler)
 	//http.ListenAndServe(":8080", nil)
 	var mux = http.NewServeMux()
-	mux.HandleFunc("/upload", uploadHandler)
+	mux.HandleFunc("/", uploadHandler)
 	//http.ListenAndServe(":8080", mux)
 	//var server = &http.Server{Addr: ":8080", Handler: mux}
 	var server = new(http.Server)
 	server.Addr = ":8080"
 	server.Handler = mux
+	fmt.Println("Ready to run http server.")
 	server.ListenAndServe()
 }
