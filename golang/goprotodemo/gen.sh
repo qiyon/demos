0befb83a7c06f18e1364b269880da4f5b72c0793
qiyon <heqiyon@gmail.com>
2019-08-01 22:02:45 +0800

    fix

--- golang/goprotodemo/gen.sh
+++ golang/goprotodemo/gen.sh
@@ -1,2 +1,2 @@
-protoc -I . ./proto/package1/*.proto  --go_out=.
-protoc -I . ./proto/package2/*.proto  --go_out=.
+protoc -I . ./proto/package1/*.proto  --go_out=plugins=grpc:.
+protoc -I . ./proto/package2/*.proto  --go_out=plugins=grpc:.
