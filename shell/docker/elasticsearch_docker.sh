afde97ba8da46b479fcc296067fed2fb534e5808
qiyon <heqiyon@gmail.com>
2019-09-02 23:29:04 +0800

    es kibana docker

--- shell/docker/elasticsearch_docker.sh
+++
@@ -1,1 +0,0 @@
-docker run --name es -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -d elasticsearch:5.6-alpine
--- shell/docker/kibana_docker.sh
+++
@@ -1,1 +0,0 @@
-docker run --name kibana -e ELASTICSEARCH_URL=http://host-ip:9200 -p 5601:5601 -d kibana:5.6
