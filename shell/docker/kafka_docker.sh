f05136ee14135adbab1279a71e048582e9892f17
qiyon <heqiyon@gmail.com>
2022-06-14 16:07:00 +0800

    feat: docker kafka cluster

--- shell/docker/kafka_docker.sh
+++ shell/docker/kafka_docker.sh
@@ -1,11 +1,29 @@
 docker run --name zookeeper \
   -p 2181:2181 \
-  -d wurstmeister/zookeeper:3.5
+  -d wurstmeister/zookeeper:latest
 
-## 关联 zookeeper，配置 zookeeper 地址
-docker run --name kafka \
-  -p 9092:9092 --link zookeeper \
-  -e KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 \
-  -e KAFKA_ADVERTISED_HOST_NAME=127.0.0.1 \
+## kafka0
+docker run --name kafka0 \
+  -p 9092:9092 \
+  -e KAFKA_ZOOKEEPER_CONNECT=host.docker.internal:2181 \
+  -e KAFKA_ADVERTISED_HOST_NAME=host.docker.internal \
   -e KAFKA_ADVERTISED_PORT=9092 \
-  -d wurstmeister/kafka:2.11-0.10.2.2
+  -d wurstmeister/kafka:2.12-2.2.0
+
+## ---- run below to be cluster ----
+
+## kafka1
+docker run --name kafka1 \
+  -p 9093:9092 \
+  -e KAFKA_ZOOKEEPER_CONNECT=host.docker.internal:2181 \
+  -e KAFKA_ADVERTISED_HOST_NAME=host.docker.internal \
+  -e KAFKA_ADVERTISED_PORT=9093 \
+  -d wurstmeister/kafka:2.12-2.2.0
+
+## kafka2
+docker run --name kafka2 \
+  -p 9094:9092 \
+  -e KAFKA_ZOOKEEPER_CONNECT=host.docker.internal:2181 \
+  -e KAFKA_ADVERTISED_HOST_NAME=host.docker.internal \
+  -e KAFKA_ADVERTISED_PORT=9094 \
+  -d wurstmeister/kafka:2.12-2.2.0
