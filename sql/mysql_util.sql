e243de4daf87d8a0e8e1f89cb7839807579935c8
qiyon <heqiyon@gmail.com>
2022-08-04 18:53:53 +0800

    feat: mysql util

---
+++ sql/mysql_util.sql
@@ -0,0 +1,20 @@
+-- datetime
+SELECT DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:%s') AS dtstr;
+SELECT NOW() AS now, UNIX_TIMESTAMP(NOW()) AS unixtime, FROM_UNIXTIME(UNIX_TIMESTAMP(NOW())) AS timestmp;
+SELECT FROM_UNIXTIME(UNIX_TIMESTAMP(NOW()), '%Y%m%d%H%i%s') AS dtnumstr;
+SELECT CONVERT_TZ(NOW(), @@session.time_zone, '+00:00') AS utcnow;
+
+-- strings
+SELECT SUBSTRING_INDEX(SUBSTRING_INDEX('{"field_1":"val","filed_2":0}', '"field_1":"', -1), '",', 1) AS split_get;
+SELECT UPPER('abc') AS strupper, LOWER('XYZ') AS strlower;
+
+-- if
+SELECT IF(TRUE, 1, 0), IF(FALSE, 1, 0);
+SELECT IFNULL(NULL, 'default_v'), IFNULL('select_v', 'default_v');
+
+-- math
+SELECT (5 MOD 2) AS 5_mod_2_get_1;
+
+-- number format
+SELECT CONCAT(CONVERT(3.1415926, DECIMAL(11, 3)), '') AS dot3f_round;
+SELECT CONCAT(CONVERT(1.0, DECIMAL(11, 3)), '') AS dot3f_zero;
