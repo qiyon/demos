8ac302ff21a4db72cf3bc2801b590cc68db8b5ce
qiyon <heqiyon@gmail.com>
2023-04-06 10:43:21 +0800

    feat: conf bash_alias

--- conf/bash_alias.sh
+++ conf/bash_alias.sh
@@ -1,4 +1,4 @@
-alias pxon="export http_proxy=http://127.0.0.1:10807; export https_proxy=http://127.0.0.1:10807; echo 'HTTP Proxy on';"
+alias pxon="export http_proxy=http://127.0.0.1:10808; export https_proxy=http://127.0.0.1:10808; echo 'HTTP Proxy on';"
 alias pxoff="unset http_proxy; unset https_proxy; echo 'HTTP Proxy off';"
 
 alias ltcp="lsof -iTCP -sTCP:LISTEN -nP"
